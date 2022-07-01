c6c5223316fe6628932e7e69a477701973e21aef
steins <dmhongcode@163.com>
2022-07-01 18:01:14 +0800

    feat:请求队列

--- src/renderer/pages/Example.tsx
+++ src/renderer/pages/Example.tsx
@@ -1,7 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Button } from 'antd';
-
+const req_yiyan = () => {
+  return axios.get('https://v.api.aa1.cn/api/yiyan/index.php');
+};
+const req_hot = () => {
+  return axios.get('https://v.api.aa1.cn/api/topbaidu/index.php');
+};
 const Example = () => {
   const [isExec, setIsExec] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
@@ -9,24 +14,42 @@ const Example = () => {
     count: 0,
     info: '',
   });
+  const [reqFuncs, setReqFuncs] = useState<Function[]>([]);
 
   useEffect(() => {
     if (!isExec) return;
-
     getOrderData();
+    // console.log(req_hot, req_yiyan);
+    // req_yiyan().then((res) => {
+    //   console.log(res);
+    // });
+    // console.log(req_hot, req_yiyan, 111);
   }, [isExec]);
 
-  const getOrderData = () => {
-    const list = axios.get('');
-    const storeData = axios.get('');
+  // const addRequest = (req: Promise<unknown>) => {
+  //   setReqFuncs([...reqFuncs, req]);
+  // };
 
-    Promise.all([list, storeData])
-      .then((values) => {
-        createRecords();
-      })
-      .catch((err) => {
-        setIsExec(false);
-      });
+  const addRequestEvt = () => {
+    setReqFuncs([...reqFuncs, req_yiyan, req_hot]);
+  };
+
+  const getOrderData = async () => {
+    console.log('getOrderData');
+    const reqArr: Promise<unknown>[] = [];
+    reqFuncs.forEach((func) => {
+      reqArr.push(func());
+    });
+    const values = await Promise.all(reqArr).catch((err) => {
+      setIsExec(false);
+    });
+    setInfoMsg({
+      count: infoMsg.count + 1,
+      info: JSON.stringify(values) + '\n' + infoMsg.info,
+    });
+    console.log(values);
+    setIsExec(false);
+    // createRecords();
   };
   const createRecords = async () => {
     const res = await axios.get('').catch((err) => {
@@ -37,13 +60,19 @@ const Example = () => {
 
   return (
     <>
-      <div>
+      <div className="m-6">
+        <Button type="primary" onClick={addRequestEvt} className="mr-4">
+          新增请求
+        </Button>
         <Button type="primary" onClick={() => setIsExec(!isExec)}>
           {isExec ? '停止执行' : '执行'}
         </Button>
         <div>
           <div>执行信息:</div>
-          <div className="max-h-40">{`执行第${infoMsg.count}次，生成 ${infoMsg.info};`}</div>
+          <div
+            className="max-h-40"
+            style={{ overflowY: 'auto' }}
+          >{`执行第${infoMsg.count}次，生成 ${infoMsg.info};`}</div>
         </div>
         <div>
           <div>错误信息:</div>
